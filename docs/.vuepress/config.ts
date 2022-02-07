623ed004ca2decba9d1652067b9c13826ad4ee2b
JararisQ <286980036@qq.com>
2022-02-07 18:12:24 +0800

    init

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,22 @@
+module.exports = {
+  title: 'Jess Github',
+  description: 'Jess Github',
+  themeConfig: {
+    logo: 'logo-mini.jpg',
+    navbar: [
+      { text: '首页', link: '/' },
+      { text: '关于', link: '/about' },
+    ],
+    // TODO 自动生成
+    sidebar: [
+      {
+        text: 'JS',
+        link: '/JS/object',
+        collapsable: true,
+        children: [
+          { text: '对象', link: '/main/JS/对象.md' }
+        ]
+      }
+    ]
+  }
+}
